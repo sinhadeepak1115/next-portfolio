473896b2ec7bf62f0d6238915ba8d4d088eaf285
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-02-10 01:34:32 +0530

    checkpoint

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -19,21 +19,22 @@ const Navbar = () => {
         ))}
       </div>
       {/* LOGO */}
-      <div className="md:hidden">
+      <div className="md:hidden lg:felx">
         <Link
           href="/"
           className='text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center'>
           <span className="text-white mr-1">Deepak</span>
           <spam className="w-12 h-8 rounded bg-white text-black flex items-center justify-center ">Sinha</spam>
         </Link>
-      </div>
-      <div className=" flex flex-row justify-between">
+      </div >
+      <div className="hidden md:flex gap-4 ">
         <Link href="https://github.com/sinhadeepak1115">
           <Image src="/github.png" alt='' width={24} height={24} />
         </Link>
         <Link href="https://twitter.com/sinhadeepak1115">
-          <Image src="/x.png" alt='' width={26} height={26} />
-        </Link>
+          <svg className="h-7 w-7" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 50 50">
+            <path d="M 11 4 C 7.134 4 4 7.134 4 11 L 4 39 C 4 42.866 7.134 46 11 46 L 39 46 C 42.866 46 46 42.866 46 39 L 46 11 C 46 7.134 42.866 4 39 4 L 11 4 z M 13.085938 13 L 21.023438 13 L 26.660156 21.009766 L 33.5 13 L 36 13 L 27.789062 22.613281 L 37.914062 37 L 29.978516 37 L 23.4375 27.707031 L 15.5 37 L 13 37 L 22.308594 26.103516 L 13.085938 13 z M 16.914062 15 L 31.021484 35 L 34.085938 35 L 19.978516 15 L 16.914062 15 z"></path>
+          </svg>      </Link>
         <Link href="">
           <Image src="/linkedin.png" alt='' width={24} height={24} />
         </Link>
@@ -60,7 +61,7 @@ const Navbar = () => {
           </div>
         }
       </div>
-    </div>
+    </div >
   )
 }
 
