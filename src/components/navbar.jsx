8c8ba33e6bb234d9aeffcc9db13f8d37169f63fb
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-02-10 05:50:16 +0530

    latest commit.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,5 @@
 "use client"
+import NavLink from "./navLink"
 import Link from "next/link"
 import { useState } from "react"
 import Image from "next/image"
@@ -12,22 +13,25 @@ const links = [
 const Navbar = () => {
   const [open, setOpen] = useState(false)
   return (
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 ">
-      <div className="hidden md:flex gap-4" >
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl ">
+      {/* LINKS */}
+      <div className="hidden md:flex gap-4 w-1/3" >
         {links.map(link => (
-          <Link href={link.url} key={link.title}>{link.title}</Link>
+          <NavLink link={link} key={link.title} />
         ))}
       </div>
       {/* LOGO */}
-      <div className="md:hidden lg:felx">
+
+      <div className="md:hidden lg:flex w-1/3  xl:justify-center">
         <Link
           href="/"
           className='text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center'>
           <span className="text-white mr-1">Deepak</span>
-          <spam className="w-12 h-8 rounded bg-white text-black flex items-center justify-center ">Sinha</spam>
+          <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">Sinha</span>
         </Link>
-      </div >
-      <div className="hidden md:flex gap-4 ">
+      </div>
+      {/* SOCIAL */}
+      <div className="hidden md:flex justify-end gap-4 w-1/3">
         <Link href="https://github.com/sinhadeepak1115">
           <Image src="/github.png" alt='' width={24} height={24} />
         </Link>
