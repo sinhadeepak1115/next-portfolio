3327d58cd45fd8a6f9c0b2722fc1c55e0e50c6cd
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-02-14 01:14:11 +0530

    push checkpoint

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -59,16 +59,17 @@ const Navbar = () => {
         ))}
       </div>
       {/* LOGO */}
-
-      <div className="md:hidden lg:flex w-1/3  xl:justify-center">
+      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
         <Link
           href="/"
-          className='text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center'>
+          className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+        >
           <span className="text-white mr-1">Deepak</span>
-          <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">Sinha</span>
+          <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
+            Sinha
+          </span>
         </Link>
-      </div>
-      {/* SOCIAL */}
+      </div>      {/* SOCIAL */}
       <div className="hidden md:flex justify-end gap-4 w-1/3">
         <Link href="https://github.com/sinhadeepak1115">
           <Image src="/github.png" alt='' width={24} height={24} />
